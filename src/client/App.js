ba25dd6eac1c47fe9dec9935bcc920a0bb1b023c
shruthidasarapu <66563360+shruthidasarapu@users.noreply.github.com>
2021-01-17 03:40:05 +0100

    added frontend part

--- src/client/App.js
+++ src/client/App.js
@@ -1,20 +1,56 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Link, Route, Switch,NavLink } from "react-router-dom";
 import TestComponent from "./components/TestComponent/TestComponent";
-
-function App() {
+import {CreateMeal} from "./components/TestComponent/createMeal";
+import {MainPage} from "./components/TestComponent/mainPage";
+import logo from './logo.png';
+//import {Form} from "./components/TestComponent/form";
+import FilterMeal from "./components/TestComponent/filterMeal";
+import {Nav,Navbar, Form,FormControl,Button, Container,Col,Image} from "react-bootstrap";
+import {ReviewDisplay} from "./components/TestComponent/review";
+import {ReservationDisplay} from "./components/TestComponent/reservation";
+import "./App.css";
+function App(){ 
   return (
+   <>
+      
+  <Navbar bg="dark" variant="dark">
+  <img src={logo} alt="Logo" style= {{height:30 , width:30}} />;
+    <Navbar.Brand href="#">MEAL-SHARING</Navbar.Brand>
+    <Nav className="mr-auto">
+      
+      <Nav.Link href="/">Home</Nav.Link>
+      <Nav.Link href="/meal">Add Meal</Nav.Link>
+      <Nav.Link href="/reservation">Reservations</Nav.Link>
+      <Nav.Link href="/review">Reviews</Nav.Link>
+    </Nav>
+    {/*<Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-info">Search</Button>
+    </Form>*/}
+  </Navbar>
+  
+
+     
     <Router>
+     
       <Route exact path="/">
-        <p>test</p>
+        <MainPage></MainPage>
       </Route>
-      <Route exact path="/lol">
-        <p>lol</p>
+      <Route exact path="/meal">
+        <CreateMeal></CreateMeal>
       </Route>
-      <Route exact path="/test-component">
-        <TestComponent></TestComponent>
-      </Route>
-    </Router>
+      <Route exact path="/meal/:id">
+        <FilterMeal></FilterMeal>
+        </Route>
+        <Route exact path="/reservation">
+     <ReservationDisplay></ReservationDisplay>
+     </Route>
+     <Route exact path="/review">
+     <ReviewDisplay></ReviewDisplay>
+     </Route>
+      </Router>
+   </> 
   );
 }
 
